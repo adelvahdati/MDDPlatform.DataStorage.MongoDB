9f5c56b08400c524de6be5f708c0c428e69abccc
Adel Vahdati <adelvahdati@gmail.com>
2025-06-01 23:49:13 +0330

    Extend MongoRepository

--- Repositories/IMongoRepository.cs
+++ Repositories/IMongoRepository.cs
@@ -1,10 +1,16 @@
+using System.Linq.Expressions;
 using MDDPlatform.DataStorage.Core;
 using MDDPlatform.SharedKernel.Entities;
+using MongoDB.Driver.Linq;
 
 namespace MDDPlatform.DataStorage.MongoDB.Repositories
 {
     public interface IMongoRepository<T, TId> : IRepository<T,TId> where T : BaseEntity<TId>
     {
-        
+        IQueryable<T> GetQueryableCollection();
+        Task<IReadOnlyList<T>> ListAsync(List<TId> Ids);
+        Task DeleteAsync(List<TId> Ids);
+        Task<bool> DeleteAsync(Expression<Func<T,bool>> predicate);
+        Task InsertOrReplaceAsync(T entity);
     }
 }
--- Repositories/MongoRepository.cs
+++ Repositories/MongoRepository.cs
@@ -1,15 +1,21 @@
 using System.Linq.Expressions;
 using MDDPlatform.SharedKernel.Entities;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace MDDPlatform.DataStorage.MongoDB.Repositories
 {
     public class MongoRepository<T, TId> : IMongoRepository<T, TId> where T : BaseEntity<TId>
     {
         protected IMongoCollection<T> Collection { get; }
+
+        
+
+
         public MongoRepository(IMongoDatabase database, string collectionName)
         {
             Collection = database.GetCollection<T>(collectionName);
+                        
         }
 
         public async Task<T> GetAsync(Expression<Func<T, bool>> predicate)
@@ -37,9 +43,42 @@ namespace MDDPlatform.DataStorage.MongoDB.Repositories
         public async Task UpdateAsync(T entity)
             => await Collection.ReplaceOneAsync(e => e.Id.Equals(entity.Id), entity);
         
-        public async Task DeleteAync(TId Id) =>
+        public async Task DeleteAsync(TId Id) =>
             await Collection.DeleteOneAsync(e=>e.Id.Equals(Id));
         public async Task DeleteAsync(T entity) =>
             await Collection.DeleteOneAsync(e => e.Id.Equals(entity.Id));
+
+        public async Task DeleteAsync(List<TId> Ids){
+            await Collection.DeleteManyAsync(e=>Ids.Contains(e.Id));
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(List<TId> Ids) =>
+            await Collection.Find(e=> Ids.Contains(e.Id)).ToListAsync();
+
+        public async Task InsertOrReplaceAsync(T entity)
+        {
+            ReplaceOptions options = new();
+            options.IsUpsert = true;
+            await Collection.ReplaceOneAsync(e=>e.Id.Equals(entity.Id),entity,options);
+        }
+        public IQueryable<T> GetQueryableCollection()
+        {
+            return Collection.AsQueryable();
+            
+        }
+
+        public async Task<bool> DeleteAsync(Expression<Func<T, bool>> predicate)
+        {
+
+            var result = await Collection.DeleteManyAsync(predicate);
+            if(result == null)
+                return false;
+
+            if(!result.IsAcknowledged)
+                return false;
+            
+            var count = result.DeletedCount;
+            return (count>0);
+        }
     }
 }
